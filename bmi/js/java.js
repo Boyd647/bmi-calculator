58bab73bc5fd1a2cd7c11d1b89ed87bc5672e1fc
Twisted-M- <43100276+Boyd647@users.noreply.github.com>
2019-04-03 11:36:50 +0200

    Add files via upload

---
+++ bmi/js/java.js
@@ -0,0 +1,21 @@
+let  bereken = document.getElementById('bereken');
+let lengte = document.getElementById('lengte');
+let gewicht = document.getElementById('gewicht');
+let resultaat = document.getElementById("getElementById");
+let input = document.getElementById("input");
+
+gewicht.addEventListener('focus', function() {
+
+})
+
+lengte.addEventListener('focus', function() {
+
+})
+
+bereken.addEventListener('focus', function() {
+
+})
+
+function clearResult() {
+  
+}
